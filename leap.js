0a6b468cbc77265d343dd1aa653c674dd595281c
dcs0spb <s.p.bradley@durham.ac.uk>
2021-05-21 15:26:14 +0100

    Basic functionality: connnect and forward frames

--- leap.js
+++ leap.js
@@ -1,8 +1,50 @@
+const Leap = require('leapjs');
+
 module.exports = function (RED) {
     'use strict';
 
-    function LeapNode (config) {
+    function LeapNode(config) {
+
+
         RED.nodes.createNode(this, config);
         const node = this;
+
+        node.controller = new Leap.Controller();
+
+        node.connected = {server: false};
+
+        node.hands = {
+            left: { entered: false, pinched: false, clenched: false },
+            right: { entered: false, pinched: false, clenched: false }
+        };
+
+        node.controller.on('connect', () => {
+            node.warn('connected to leap server');
+            connected('server', true);
+         });
+
+         node.controller.on('disconnect', () => {
+            node.warn('disconnected from leap server');
+            connected('server', false);
+         });
+
+         node.controller.on('frame', (frame) => {
+             let msg = [{payload: frame}, null, null];
+             node.send(msg);
+         })
+         
+        node.controller.connect();
+
+        function connected(thing, isConnected){
+            node.connected[thing] = isConnected;
+            if (node.connected['server']) {
+                node.status({fill:"green",shape:"dot",text:"connected"});
+            } else {
+                node.status({fill:"red",shape:"ring",text:"disconnected"});
+            }
+        }
     }
+
+    RED.nodes.registerType("leap", LeapNode)
 }
+
